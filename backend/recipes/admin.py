2ac966767a558445c3e68204fe3e3460b9785cad
Ramzan Elikhanov <ramzan.elikhanov@gmail.com>
2024-12-29 09:23:53 +0300

    fix short link in admin panel

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -26,16 +26,22 @@ class RecipeIngredientInline(admin.StackedInline):
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     form = RecipeForm
-    list_display = ('name', 'author', 'short_link')
+    list_display = ('name', 'author')
     readonly_fields = ('favorite_count', 'get_short_link')
     exclude = ('short_link', )
     search_fields = ('name', 'author__username')
     list_filter = ('tags', )
     inlines = (RecipeIngredientInline,)
 
+    def get_queryset(self, request):
+        self.request = request  # Сохраняем request для дальнейшего использования
+        return super().get_queryset(request)
+
     def get_short_link(self, obj):
-        if obj.pk:
-            return obj.short_link
+        request = self.request  # Получаем request из Admin
+        if request and obj.pk and obj.short_link:
+            return request.build_absolute_uri(f'/s/{obj.short_link}')
+
         return 'Ссылка появится после сохранения'
 
     get_short_link.short_description = 'Короткая ссылка'
